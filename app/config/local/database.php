e74ad6bcf650d9b90375a16afd152fc4e6115a67
Khandaker Azizur Rahman <azizur77@yahoo.com>
2014-09-07 17:08:53 +0600

    3rd checkin- before migration

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'l4todo',
+			'username'  => 'root',
+			'password'  => 'sangu',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
